310c473e5b95a73affc5030a00fe089519efb18c
aaron00101010 <r23qw@hotmail.com>
2023-11-11 18:08:03 +0800

    fix: bundle error

--- .vitepress/theme/components/PostHeader.vue
+++ .vitepress/theme/components/PostHeader.vue
@@ -1,8 +1,10 @@
 <template>
-  <h3 class="font-bold mb-4 tracking-tight text-3xl text-[var(--vp-c-text-1)] leading-8">{{ frontmatter.title }}</h3>
-  <p v-if="frontmatter.date" class="font-medium pt-4 text-[var(--vp-c-text-2)] leading-6">
-    {{ new Date(frontmatter.date).toLocaleDateString() }}
-  </p>
+  <div class="mb-6">
+    <h3 class="font-bold tracking-tight text-3xl text-[var(--vp-c-text-1)] leading-8">{{ frontmatter.title }}</h3>
+    <p v-if="frontmatter.date" class="font-medium pt-4 text-[var(--vp-c-text-2)] leading-6">
+      {{ new Date(frontmatter.date).toLocaleDateString() }}
+    </p>
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/about.md
+++ src/about.md
@@ -9,4 +9,4 @@ let nickname = "Aaron00101010"
 ```
 欢迎关注微信公众号 **摇滚Coder**  
 
-![摇滚Coder](/wechat-qrcode.bmp)
+![摇滚Coder](./public/wechat-qrcode.jpg)
